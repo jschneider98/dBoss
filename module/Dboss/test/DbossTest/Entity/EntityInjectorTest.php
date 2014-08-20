c351ab27595e36478398055048daf9bcacbf69df
James Schneider <jschneider98@gmail.com>
2014-08-20 14:28:43 -0700

    Added post load test

--- module/Dboss/test/DbossTest/Entity/EntityInjectorTest.php
+++ module/Dboss/test/DbossTest/Entity/EntityInjectorTest.php
@@ -3,6 +3,7 @@ namespace DbossTest\Entity;
 
 use Zend\ServiceManager\ServiceManager;
 use Dboss\Entity\EntityInjector;
+use Dboss\Entity\User;
 use PHPUnit_Framework_TestCase;
 
 class EntityInjectorTest extends PHPUnit_Framework_TestCase
@@ -21,6 +22,47 @@ class EntityInjectorTest extends PHPUnit_Framework_TestCase
             $service_manager,
             $entity_injector->getServiceManager(),
             "Service Manager not set correctly"
+        );
+    }
+
+    /**
+     * 
+     */
+    public function testPostLoad()
+    {
+        $security = array(
+            "salt_key"        => "unit_test_salt",
+            'iteration_count' => 8,
+            'portable_hashes' => 0,
+        );
+
+        $user = new User();
+
+        $event_mock = $this->getMock(
+            'Doctrine\ORM\Event\LifecycleEventArgs',
+            array(),
+            array(),
+            "",
+            false
+        );
+
+        $event_mock->expects($this->any())
+            ->method('getEntity')
+            ->will($this->returnValue($user));
+
+        $sm_mock = $this->getMock('Zend\ServiceManager\ServiceManager');
+
+        $sm_mock->expects($this->any())
+            ->method('get')
+            ->will($this->returnValue(array('security' => $security)));
+
+        $entity_injector = new EntityInjector($sm_mock);
+        $entity_injector->postLoad($event_mock);
+
+        $this->assertSame(
+            $security,
+            $user->security,
+            "Post Load did not properly inject the security value into user entity"
         );
     }
 }
