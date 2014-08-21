cb290631499e856579699b1aff042244c47761ef
James Schneider <jschneider98@gmail.com>
2014-08-21 10:01:53 -0700

    Added testAuthenticateCredentialInvalid

--- module/Dboss/test/DbossTest/Auth/AdapterTest.php
+++ module/Dboss/test/DbossTest/Auth/AdapterTest.php
@@ -40,4 +40,43 @@ class AdapterTest extends PHPUnit_Framework_TestCase
             "Result code should match FAILURE_IDENTITY_NOT_FOUND"
         );
     }
+
+    /**
+     * 
+     */
+    public function testAuthenticateCredentialInvalid()
+    {
+        $user_mock = $this->getMock('\Dboss\Entity\User');
+        $user_mock->expects($this->any())
+            ->method('checkPassword')
+            ->will($this->returnValue(false));
+
+        $user_service_mock = $this->getMock(
+            '\Dboss\Service\UserService',
+            array(),
+            array(),
+            '',
+            false
+        );
+
+        $user_service_mock->expects($this->any())
+            ->method('findByUserName')
+            ->will($this->returnValue($user_mock));
+
+        $params = array(
+            'user_service' => $user_service_mock,
+            'user_name'    => 'unit_test_no_user',
+            'password'     => 'password',
+        );
+
+        $adapter = new Adapter($params);
+        $result = $adapter->authenticate();
+
+
+        $this->assertSame(
+            Result::FAILURE_CREDENTIAL_INVALID,
+            $result->getCode(),
+            "Result code should match FAILURE_CREDENTIAL_INVALID"
+        );
+    }
 }
