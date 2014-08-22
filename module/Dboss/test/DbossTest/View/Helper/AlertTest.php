68598bf3a6f2ab3c8a21253cbc6a8f5fe4d95449
James Schneider <jschneider98@gmail.com>
2014-08-21 19:09:59 -0700

    Added testInvokeArray

--- module/Dboss/test/DbossTest/View/Helper/AlertTest.php
+++ module/Dboss/test/DbossTest/View/Helper/AlertTest.php
@@ -9,11 +9,27 @@ class AlertTest extends PHPUnit_Framework_TestCase
     /**
      * 
      */
-    public function testInvoke()
+    public function testInvokeMsg()
     {
         $alert = new Alert();
 
         $result = $alert->__invoke("test", "info", "unit_test_title");
+
+        $this->assertSame(
+            true,
+            is_string($result),
+            "Returned value should be a string"
+        );
+    }
+
+    /**
+     * 
+     */
+    public function testInvokeArray()
+    {
+        $alert = new Alert();
+
+        $result = $alert->__invoke(array("test"), "info", "unit_test_title");
 
         $this->assertSame(
             true,
