daaf4c64e943eb8d2a9a11b73b680ddbcf3e83c4
James Schneider <jschneider98@gmail.com>
2014-08-22 18:51:38 -0700

    Added DataTypeServiceTest

---
+++ module/Dboss/test/DbossTest/Service/DataTypeServiceTest.php
@@ -0,0 +1,26 @@
+<?php
+namespace DbossTest\Service;
+
+use DbossTest\Bootstrap;
+use Dboss\Service\DataTypeService;
+use PHPUnit_Framework_TestCase;
+
+class DataTypeServiceTest extends PHPUnit_Framework_TestCase
+{
+    protected $service_manager;
+
+    protected function setUp()
+    {
+        $this->service_manager = Bootstrap::getServiceManager();
+    }
+
+    /**
+     * 
+     */
+    public function testRoleServiceConstruct()
+    {
+        $object_manager = $this->service_manager->get('Doctrine\ORM\EntityManager');
+        $params = array('object_manager' => $object_manager);
+        $data_type_service = new DataTypeService($params);
+    }
+}
