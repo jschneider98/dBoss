be1fdc6e384c32d40a7178e7e91f52c01e24deaa
James Schneider <jschneider98@gmail.com>
2014-08-23 17:40:02 -0700

    Added AuthFilterTest

---
+++ module/Dboss/test/DbossTest/Form/AuthFilterTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace DbossTest\Form;
+
+use Dboss\Form\AuthFilter;
+use PHPUnit_Framework_TestCase;
+
+class AuthFilterTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * 
+     */
+    public function testAuthFiler()
+    {
+        $auth_filter = new AuthFilter();
+
+        $this->assertSame(2, $auth_filter->count());
+
+        $this->assertTrue($auth_filter->has('user_name'));
+        $this->assertTrue($auth_filter->has('password'));
+    }
+}
