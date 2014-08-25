6003a7ae07faf8c60833508d9692cd69f7428bf9
James Schneider <jschneider98@gmail.com>
2014-08-25 12:00:02 -0700

    Added testAddVerifyPassword

--- module/Dboss/test/DbossTest/Form/UserFormTest.php
+++ module/Dboss/test/DbossTest/Form/UserFormTest.php
@@ -125,4 +125,19 @@ class UserFormTest extends PHPUnit_Framework_TestCase
             'password field missing from form'
         );
     }
+
+    /**
+     * 
+     */
+    public function testAddVerifyPasswrod()
+    {
+        $form = new UserForm();
+        $form->addVerifyPassword();
+
+        $this->assertSame(
+            true,
+            $form->has('verify_password'),
+            'verify_password field missing from form'
+        );
+    }
 }
