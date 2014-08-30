a035527d48ed0328a09527bd07059b7c70748ffc
James Schneider <jschneider@Jamess-MacBook-Pro.local>
2014-08-30 10:00:27 -0700

    Added QueryRunnerTest

---
+++ module/Dboss/test/DbossTest/QueryRunnerTest.php
@@ -0,0 +1,17 @@
+<?php
+namespace DbossTest;
+
+use Dboss\QueryRunner;
+use PHPUnit_Framework_TestCase;
+
+class QueryRunnerTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     *
+     */
+    public function testContructNoUser()
+    {
+        $this->setExpectedException('\Exception');
+        $runner = new QueryRunner(array());
+    }
+}
